266e00f5bcb72da196354b54d19867dda39f0cf9
Aidan Bleser <aidanbleser35@gmail.com>
2025-05-29 11:18:53 -0500

    feat(ui/copy-button): Allow the button to have text

---
+++ src/routes/components/copy-button/with-text.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { CopyButton } from '$lib/components/ui/copy-button';
+	import { TerminalIcon } from '@lucide/svelte';
+
+    const command = 'jsrepo add ui/copy-button'
+</script>
+
+<CopyButton text={command} size="sm" variant="outline">
+	{#snippet icon()}
+		<TerminalIcon />
+	{/snippet}
+    <span class="text-sm font-mono">{command}</span>
+</CopyButton>   
