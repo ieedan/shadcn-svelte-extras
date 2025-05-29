37568b9077a3e01bbea7548a1220393bdf6b181a
Aidan Bleser <aidanbleser35@gmail.com>
2025-05-29 10:47:43 -0500

    improve docs

--- src/routes/components/modal/basic.svelte
+++ src/routes/components/modal/basic.svelte
@@ -3,24 +3,29 @@
 	import { Button, buttonVariants } from '$lib/components/ui/button';
 	import { Label } from '$lib/components/ui/label';
 	import { Input } from '$lib/components/ui/input';
-
-	let open = $state(false);
 </script>
 
-<Modal.Root bind:open>
-	<Modal.Trigger class={buttonVariants({ variant: 'outline' })}>Open Modal</Modal.Trigger>
+<Modal.Root>
+	<Modal.Trigger class={buttonVariants({ variant: 'outline' })}>Edit Profile</Modal.Trigger>
 	<Modal.Content>
 		<Modal.Header>
-			<Modal.Title>Create a team</Modal.Title>
-			<Modal.Description>Create a team</Modal.Description>
+			<Modal.Title>Edit Profile</Modal.Title>
+			<Modal.Description>
+				Make changes to your profile here. Click save when you're done.
+			</Modal.Description>
 		</Modal.Header>
-		<div>
-			<Label>Team Name</Label>
-			<Input />
+		<div class="flex flex-col gap-2">
+			<div>
+				<Label for="name">Name</Label>
+				<Input id="name" value="Pedro Duarte" />
+			</div>
+			<div>
+				<Label for="username">Username</Label>
+				<Input id="username" value="@peduarte" />
+			</div>
 		</div>
 		<Modal.Footer>
-			<Button variant="outline" onclick={() => (open = false)}>Cancel</Button>
-			<Button>Continue</Button>
+			<Button>Save Changes</Button>
 		</Modal.Footer>
 	</Modal.Content>
 </Modal.Root>
