b0aa8c1704a4140c7b24f84ac23686039826f189
Aidan Bleser <aidanbleser35@gmail.com>
2025-06-04 14:23:38 -0500

    format

--- src/lib/components/docs/api-reference/api-reference.svelte
+++ src/lib/components/docs/api-reference/api-reference.svelte
@@ -3,10 +3,11 @@
 	import type { Component } from './api-reference';
 	import ReferenceTable from './reference-table.svelte';
 
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	let { name, components }: { name: string; components: Record<string, Component<any>> } = $props();
 </script>
 
 <Subheading>API Reference</Subheading>
-{#each Object.values(components) as component}
+{#each Object.values(components) as component (component.name)}
 	<ReferenceTable {name} {component} />
 {/each}
