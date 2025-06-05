3baa34fc87745911d43fe9f04f4a73f3426b63d8
Aidan Bleser <aidanbleser35@gmail.com>
2025-06-05 07:49:53 -0500

    field-set updates

--- src/lib/components/ui/field-set/field-set.svelte
+++ src/lib/components/ui/field-set/field-set.svelte
@@ -1,28 +1,17 @@
 <script lang="ts">
 	import { cn } from '$lib/utils/utils';
-	import type { WithElementRef } from 'bits-ui';
-	import type { HTMLAttributes } from 'svelte/elements';
-	import { tv, type VariantProps } from 'tailwind-variants';
+	import { fieldSetVariants } from '.';
+	import type { FieldSetRootProps } from './types';
 
-	const style = tv({
-		base: 'border-border flex h-fit w-full flex-col rounded-lg border',
-		variants: {
-			variant: {
-				default: 'border-border bg-card',
-				destructive: 'border-destructive'
-			}
-		}
-	});
-
-	type Variant = VariantProps<typeof style>['variant'];
-
-	interface Props extends WithElementRef<HTMLAttributes<HTMLDivElement>> {
-		variant?: Variant;
-	}
-
-	let { variant = 'default', children, class: className, ...restProps }: Props = $props();
+	let {
+		ref = $bindable(null),
+		variant = 'default',
+		children,
+		class: className,
+		...rest
+	}: FieldSetRootProps = $props();
 </script>
 
-<div class={cn(style({ variant }), className)} {...restProps}>
+<div bind:this={ref} class={cn(fieldSetVariants({ variant }), className)} {...rest}>
 	{@render children?.()}
 </div>
