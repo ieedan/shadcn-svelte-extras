5cf0df45b71b1586402736c5cc7e1d2968b81641
Aidan Bleser <aidanbleser35@gmail.com>
2025-11-16 14:12:37 -0600

    better

--- src/routes/demos/[...path]/+page.svelte
+++ src/routes/demos/[...path]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Delayed from '$lib/components/delayed.svelte';
 	import { Spinner } from '$lib/components/ui/spinner/index.js';
 
 	let { data } = $props();
@@ -8,7 +9,12 @@
 
 <div class="flex min-h-svh place-items-center justify-center">
 	{#await ComponentPromise}
-		<span class="flex items-center gap-2 text-muted-foreground"><Spinner/> Loading...</span>
+		<!-- delay the loading state just a bit to make it seem faster -->
+		<Delayed delay={1000}>
+			<span class="text-muted-foreground flex items-center gap-2">
+				<Spinner /> Loading...
+			</span>
+		</Delayed>
 	{:then { default: Component }}
 		<Component />
 	{/await}
