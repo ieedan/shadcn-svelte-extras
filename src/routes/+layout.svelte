81b3741b0652b78c74716a7e98c22ce1ec3ff5ae
Aidan Bleser <aidanbleser35@gmail.com>
2026-03-30 08:41:27 -0500

    fix meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,14 @@
 	import '../app.css';
 	import { page } from '$app/state';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
-	import { MetaTags } from '$lib/components/site/meta-tags';
+	import { MetaTags, deepMerge } from 'svelte-meta-tags';
 	import * as Add from '$lib/components/ui/add';
 	import { REGISTRY_OPTIONS } from '$lib/constants';
 	import { PersistedState } from 'runed';
 
-	let { children } = $props();
+	let { data, children } = $props();
+
+	let metaTags = $derived(deepMerge(data.baseMetaTags, page.data.pageMetaTags ?? {}));
 
 	const agent = new PersistedState<Add.Agent>('user-agent-preference', 'npm');
 	const registry = new PersistedState<(typeof REGISTRY_OPTIONS)[number]>(
@@ -17,45 +19,7 @@
 	);
 </script>
 
-<MetaTags
-	title={'shadcn-svelte-extras'}
-	description={'Finish your app with awesome svelte components'}
-	keywords={[
-		'shadcn-svelte',
-		'extras',
-		'svelte',
-		'components',
-		'cli',
-		'jsrepo',
-		'mcp',
-		'phone-input',
-		'tags-input',
-		'star-rating',
-		'file-drop-zone'
-	]}
-	twitter={{
-		cardType: 'summary_large_image',
-		title: `shadcn-svelte-extras`,
-		description: 'Finish your app with awesome svelte components',
-		image: 'https://shadcn-svelte-extras.com/og.png',
-		creator: '@ieeeedan'
-	}}
-	openGraph={{
-		url: page.url.toString(),
-		type: 'website',
-		title: `shadcn-svelte-extras`,
-		description: 'Finish your app with awesome svelte components',
-		siteName: 'shadcn-svelte-extras',
-		images: [
-			{
-				url: 'https://shadcn-svelte-extras.com/og.png',
-				width: 2014,
-				height: 1143,
-				alt: 'shadcn-svelte-extras - Finish your app.'
-			}
-		]
-	}}
-/>
+<MetaTags {...metaTags} />
 
 <ModeWatcher />
 <Toaster />
