d4319071e8cff2f44f1875a2eb74ecfa0b30d56c
Aidan Bleser <aidanbleser35@gmail.com>
2026-04-20 07:18:05 -0500

    demo

--- src/lib/demos/split-button.svelte
+++ src/lib/demos/split-button.svelte
@@ -3,5 +3,30 @@
 </script>
 
 <SplitButton.Root>
-	<!-- Implement primary action and dropdown trigger here -->
+	<SplitButton.Action value="merge">Merge Changes</SplitButton.Action>
+	<SplitButton.Action value="squash">Squash and merge</SplitButton.Action>
+	<SplitButton.Action value="rebase">Rebase and merge</SplitButton.Action>
+	<SplitButton.Select>
+		<SplitButton.SelectTrigger />
+		<SplitButton.SelectContent>
+			<SplitButton.SelectAction value="merge" class="flex flex-col gap-0.5">
+				<span>Create a merge commit</span>
+				<span class="text-muted-foreground text-xs">
+					All commits from this branch will be added to the base branch via a merge commit.
+				</span>
+			</SplitButton.SelectAction>
+			<SplitButton.SelectAction value="squash" class="flex flex-col gap-0.5">
+				<span>Squash and merge</span>
+				<span class="text-muted-foreground text-xs">
+					The 2 commits from this branch will be combined into one commit in the base branch.
+				</span>
+			</SplitButton.SelectAction>
+			<SplitButton.SelectAction value="rebase" class="flex flex-col gap-0.5">
+				<span>Rebase this branch onto the base branch</span>
+				<span class="text-muted-foreground text-xs">
+					The 2 commits from this branch will be rebased and added to the base branch.
+				</span>
+			</SplitButton.SelectAction>
+		</SplitButton.SelectContent>
+	</SplitButton.Select>
 </SplitButton.Root>
